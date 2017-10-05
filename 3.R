00d045589142d2e6d767b935a8dd0f6d6f93249c
Gergely Daroczi (@daroczig) <daroczig@rapporter.net>
2017-10-05 14:04:33 +0200

    add answers to the questions

--- 3.R
+++ 3.R
@@ -11,8 +11,25 @@ str(hotels)
 ## #############################################################################
 
 ## TODO compute required budget (EUR) to try all hotels for a night
+hotels[, sum(price_HUF)]
+hotels[, sum(price_HUF) / 310]
+hotels$price_EUR <- hotels$price_HUF / 310
+hotels[, price_EUR := price_HUF / 310]
+hotels[, sum(price_EUR)]
 
 ## TODO categorize hotels into 3 buckets by price
+hotels[, pricecat := cut(price_EUR, 3)]
+hotels[, .N, by = pricecat]
+hotels[, pricecat := cut(price_EUR, 3, dig.lab = 8)]
+hotels[, .N, by = pricecat]
+
+hotels[, pricecat := cut(price_EUR, c(0, 10000, 100000, Inf), dig.lab = 8)]
+hotels[, .N, by = pricecat]
+
+hotels[, pricecat := cut(price_EUR,
+                         c(0, 10000, 100000, Inf),
+                         labels = c('cheap', 'average', 'expensive'))]
+hotels[, .N, by = pricecat]
 
 ## stats approach to categorize hotels by price: using average and std deviation
 avg_price <- hotels[, mean(price_EUR)]
@@ -26,6 +43,9 @@ hotels[, .N, by = pricecat]
 ## also very different by city
 
 ## TODO avg price in EUR per city
+hotels[, mean(price_EUR), by = city]
+## use named expressions to name columns
+hotels[, list(avg_price = mean(price_EUR)), by = city]
 
 ## #############################################################################
 ## summaries
@@ -44,10 +64,17 @@ hotels[, pricecat := cut(price_EUR, c(0,
 hotels[, .N, by = pricecat]
 
 ## TODO compute the number of hotels in the city
+hotels[, .(hotels = .N), by = city]
 
 ## TODO merge this metric to the actual dataset
+hotels[, hotels := .N, by = city]
 
 ## TODO create a new variable on city type: small or big depending on the number of hotels
+hotels[, .N, by = city]
+hotels[, citytype := cut(hotels, 2)]
+hotels[, .N, by = citytype]
+hotels[, citytype := cut(hotels, 2, labels = c('small', 'big'))]
+hotels[, .N, by = citytype]
 
 ## number of cases per multiple variables
 hotels[, .N, by = list(pricecat, citytype)]
@@ -68,10 +95,29 @@ price_per_type
 hotels[, .(price_avg = mean(price_EUR)), by = city]
 
 ## TODO min, avg, median and max price in EUR per city
+hotels[, list(
+    min_price = min(price_EUR),
+    avg_price = mean(price_EUR),
+    med_price = median(price_EUR),
+    max_price = max(price_EUR)
+), by = city]
 
 ## TODO round it up to EUR
+hotels[, list(
+    min_price = round(min(price_EUR)),
+    avg_price = round(mean(price_EUR)),
+    med_price = round(median(price_EUR)),
+    max_price = round(max(price_EUR))
+), by = city]
 
 ## TODO compute the average price, rating, stars per city in a new dataset
+hotels[, .(price_avg = mean(price_EUR),
+           rating_avg = mean(rating),
+           stars_avg = mean(stars)), by = city]
+
+hotels[, .(price_avg = mean(price_EUR),
+           rating_avg = mean(rating, na.rm = TRUE),
+           stars_avg = mean(stars, na.rm = TRUE)), by = city]
 
 ## check the same on rating and stars
 hotels[, lapply(.SD, mean), by = city, .SDcols = c('price_EUR', 'rating', 'stars')]
@@ -142,6 +188,8 @@ p + facet_wrap( ~ citytype)
 p + facet_grid(citytype ~ pricecat)
 
 ## TODO fix order of the labels
+hotels[, pricecat := factor(pricecat, levels = c('below avg', 'avg', 'above avg'))]
+p + facet_grid(citytype ~ pricecat)
 
 ## stacked, clustered bar charts
 p <- ggplot(hotels, aes(x = pricecat, fill = citytype))
@@ -196,137 +244,30 @@ p + theme_custom() + scale_fill_brewer(palette = "Greens")
 ## see more examples at http://docs.ggplot2.org/dev/vignettes/themes.html
 
 ## TODO plot a barplot on the number of hotels per city type
+ggplot(hotels, aes(citytype)) + geom_bar()
+
+## more difficult approach with custom summary
+ggplot(hotels[, .N, by = citytype], aes(citytype, y = N)) + geom_bar(stat = 'identity')
+
 ## TODO plot a histogram on the prices in EUR
-## TODO plot a histogram on the prices in EUR split by city type
+ggplot(hotels, aes(price_EUR)) + geom_histogram()
+
+## TODO plot a histogram on the prices in EUR
+##      split by city type
+ggplot(hotels, aes(price_EUR)) + geom_histogram() +
+    facet_wrap(~ citytype)
+
 ## TODO plot a boxplot on the prices in EUR split by city type
-## TODO plot a scatterplot on the prices in EUR and the distance from city center
+ggplot(hotels, aes(citytype, price_EUR)) + geom_boxplot()
+
+## TODO plot a scatterplot on the prices in EUR and
+## the distance from city center
+ggplot(hotels, aes(price_EUR, dist_center_km)) + geom_point()
+
 ## TODO add a model to the previous plot
+ggplot(hotels, aes(dist_center_km, price_EUR)) +
+    geom_point() + geom_smooth(method = 'lm')
+
 ## TODO plot a boxplot on the prices in EUR split by cat(rating)
-
-## #############################################################################
-## creating new variables: parsing text
-## #############################################################################
-
-## TODO avg price in EUR per country
-
-## parsing the country out of the city column
-hotels[, city := as.character(city)]
-
-x <- 'Budapest, Hungary'
-strsplit(x, ', ')
-str(strsplit(x, ', '))
-strsplit(x, ', ')[[1]]
-strsplit(x, ', ')[[1]][2]
-
-hotels[, country := strsplit(city, ', ')[[1]][2], by = city]
-
-sub('.*, ', '', x)
-hotels[, country := sub('.*, ', '', city)]
-
-hotels[, sum(price_EUR), by = country]
-
-## TODO compute the avg price in Hungary rating > 4.5
-
-## TODO histogram on the same prices
-
-## more summaries => percentage of high rated hotels in Hungary
-hotels[country == 'Hungary', sum(rating > 4.5, na.rm = TRUE)]
-hotels[country == 'Hungary', sum(rating > 4.5, na.rm = TRUE) / .N]
-hotels[country == 'Hungary', sum(rating > 4.5, na.rm = TRUE) / .N * 100]
-
-## #############################################################################
-## joining external data
-## #############################################################################
-
-library(XML)
-gdp <- readHTMLTable(readLines('https://en.wikipedia.org/wiki/List_of_countries_by_GDP_(PPP)_per_capita'), which = 3)
-gdp <- readHTMLTable(readLines('http://bit.ly/CEU-R-gdp'), which = 3)
-head(gdp)
-
-## convert to data.table
-gdp <- data.table(gdp)
-gdp <- as.data.table(gdp)
-setDT(gdp)
-
-gdp[, country := iconv(`Country/Territory`, to = 'ASCII', sub = '')]
-gdp[, gdp := as.numeric(sub(',', '', `Int$`))]
-
-countries <- hotels[, unique(country)]
-countries %in% gdp$country
-## \o/
-
-merge(hotels, gdp, by = 'country')
-gdp <- gdp[, .(country, gdp)]
-hotels <- merge(hotels, gdp, by = 'country')
-
-## TODO compute the avg price of hotels and the GDP per country
-
-## TODO visualize association
-
-## TODO which are the countries with relatively low GDP but high hotel prices?
-
-## let's exclude these
-country_stats[price < 200, .(gdp, price)]
-cor(country_stats[price < 200, .(gdp, price)])
-ggplot(country_stats[price < 200, .(gdp, price)],
-       aes(gdp, price)) + geom_point() + geom_smooth(method = 'lm')
-
-## #############################################################################
-## restructure long and wide tables
-## #############################################################################
-
-library(reshape2)
-
-cities <- hotels[, list(
-    min_price = min(price_EUR),
-    avg_price = mean(price_EUR),
-    med_price = median(price_EUR),
-    max_price = max(price_EUR)
-), by = city]
-str(cities)
-
-cities <- melt(cities)
-str(cities)
-
-cities <- melt(cities, id.vars = 'city')
-str(cities)
-cities
-
-## #############################################################################
-## revisit aggregates for dataviz
-## #############################################################################
-
-ggplot(hotels, aes(country, price_HUF)) + geom_boxplot()
-
-ggplot(hotels, aes(country, price_HUF)) + geom_boxplot() +
-    theme(axis.text.x = element_text(angle = 45, hjust = 1))
-
-library(scales)
-ggplot(hotels, aes(country, price_HUF)) + geom_boxplot() +
-    theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-    scale_y_continuous(labels = dollar_format(suffix = 'Ft', prefix = '')) +
-    xlab('') + ylab('') + ggtitle('Hotel prices on Dec 31')
-
-## TODO show the ratings per country on a boxplot
-
-## TODO show the average rating per country
-
-## custom ggplot theme
-mytheme <- function() {
-    theme(axis.text.x = element_text(angle = 45, hjust = 1),
-          panel.background = element_rect(fill = 'snow1'))
-}
-ggplot(ratings, aes(country, avg_rating, color)) + geom_bar(stat = 'identity') + mytheme()
-
-## TODO plot the min, max and avg price per country
-
-## move and rename legend
-ggplot(countries, aes(country, value, color = variable, group = variable)) +
-    geom_line() + mytheme() +
-    theme(legend.position = 'top') + scale_color_brewer('Metrics', palette = 'Greens')
-
-## using facets
-ggplot(countries, aes(country, value, group = 1)) +
-    geom_line() + mytheme() + facet_grid(variable ~ .)
-ggplot(countries, aes(country, value, group = 1)) +
-    geom_line() + mytheme() + facet_grid(variable ~ ., scales = 'free')
+hotels[, ratingcat := cut(rating, 5)]
+ggplot(hotels, aes(ratingcat, price_EUR)) + geom_boxplot()
